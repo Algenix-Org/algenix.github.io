316c5b75328104c1d21aa860f3d365ab6e2a8a64
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-01-18 07:21:49 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+  output: "export",
+  // Add the base path if the site is served from a subdirectory
+  basePath: "/https://github.com/Algenix-Org/algenix.github.io",
+  assetPrefix: "/https://github.com/Algenix-Org/algenix.github.io",
+};
