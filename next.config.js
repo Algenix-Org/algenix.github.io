cfa3dc19d2fe48ee9510ceac0374cafb2f55c172
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-04-09 06:51:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
-  basePath: "/algenix.github.io", // Subdirectory for GitHub Pages
-  assetPrefix: "/algenix.github.io", // Prefix for static assets
+  basePath: "/algenix.github.io",
 };
