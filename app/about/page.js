09ee13a8e0acdae5eef279188cc90885531737e1
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-06-17 10:15:41 -0300

    Delete app directory

--- app/about/page.js
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>About Us</h1>
-      <p>This is the about page of the template site.</p>
-      <a href="/algenix.github.io">Back to Home</a>
-    </div>
-  );
-}
--- app/pages.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Welcome to My Next.js Site</h1>
-      <p>This is a simple template deployed to GitHub Pages.</p>
-      <a href="/algenix.github.io/about">Go to About Page</a>
-    </div>
-  );
-}
